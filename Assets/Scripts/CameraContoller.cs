8a6beddd9a066da3ed4e13683ac2af02802c0979
Rafael Briet <rafael-briet@hotmail.com>
2022-06-06 16:06:36 -0300

    Mouse position offsets camera position

--- Assets/Scripts/CameraContoller.cs
+++ Assets/Scripts/CameraContoller.cs
@@ -6,9 +6,24 @@ public class CameraContoller : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
+    [SerializeField]
+    private Vector2 targetOffset;
 
     private void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        Vector3 mouseScreenPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        mouseScreenPosition.x = (mouseScreenPosition.x - 0.5f) * 2f;
+        mouseScreenPosition.y = (mouseScreenPosition.y - 0.5f) * 2f;
+
+        mouseScreenPosition.x = Mathf.Clamp(mouseScreenPosition.x, -1f, 1f);
+        mouseScreenPosition.y = Mathf.Clamp(mouseScreenPosition.y, -1f, 1f);
+
+        float offsetX = targetOffset.x * mouseScreenPosition.x;
+        float offsetY = targetOffset.y * mouseScreenPosition.y;
+
+        Vector3 cameraPosition = new Vector3(target.position.x + offsetX, target.position.y + offsetY, transform.position.z);
+
+        transform.position = cameraPosition;
     }
 }
