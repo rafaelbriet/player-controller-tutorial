1b181d4ba93f8369cf52571bf0a306cbc351e14e
Rafael Briet <rafael-briet@hotmail.com>
2022-06-06 16:04:24 -0300

    Camera follows player

---
+++ Assets/Scripts/CameraContoller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContoller : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
